b3363fa30d9b2027b5f4e3f5a88360c4797171fa
wjwalcher <wjwalcher@gmail.com>
2019-12-06 21:12:38 -0800

    Update script to generate simple test graph

--- graphgen.py
+++ graphgen.py
@@ -17,6 +17,8 @@ def plot_figure(title, xlabel, ylabel, xticks=None):
 def create_bar_chart(ax, fig, xdata, ydata, xticks=None):
     ax.bar(xdata, ydata, tick_label=xticks, log=True)
 
+
+
 ydata_dram = [183897.16, 187320.044, 181281.336]
 ydata_pmem = [185642.472, 189318.422, 185432.812]
 xticks=["4K", "2M", "1G"]
@@ -37,5 +39,20 @@ create_bar_chart(ax_bar_fio_pmem_1g, fig_bar_fio_pmem_1g, [1, 2, 3], ydata_fio_1
 # TODO: PMEM 4K Read FIO 
 
 # TODO: DRAM 4K Read FIO 
+
+# dTLB Misses for Simple Test
+ax_simple_test, fig_simple_test = plot_figure("dTLB Misses For Simple Test", "Page Size", "dTLB Misses that Cause Page Walk")
+ydata_simple_test_dram = [129032242, 88692536, 26425]
+ydata_simple_test_pmem = [124648518, 82780288, 32855]
+index = np.arange(3)
+bar_width = 0.35
+opacity = 0.9
+ax_simple_test.bar(index, ydata_simple_test_dram, bar_width, alpha=opacity, color='r', label ='DRAM', log=True)
+ax_simple_test.bar(index+bar_width, ydata_simple_test_pmem, bar_width, alpha=opacity, color='b', label='PMEM', log=True)
+ax_simple_test.set_xlabel('Page Size')
+ax_simple_test.set_ylabel('dTLB Misses Causing a Page Table Walk')
+ax_simple_test.set_xticks(index+bar_width/2)
+ax_simple_test.set_xticklabels(('4K', '2M', '1G'))
+ax_simple_test.legend()
 
 plt.show()
