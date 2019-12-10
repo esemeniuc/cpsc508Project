2330b9dfa112898584d3baa59d687db1ada82e41
cs508 <cs508@hennessy.cs.ubc.ca>
2019-12-10 14:22:57 -0800

    scripted apexmap run

--- apex-map/apex-map/script.sh
+++ apex-map/apex-map/script.sh
@@ -1,10 +1,17 @@
 # baic loop
 
-for i in {0..1}
-	  do 
-		  gcc gen.pub.c -lm
-		  ./a.out
-		  gcc Apex.c -lm
-		  sudo perf stat -B -d -d -d ./a.out || rawResults.txt
+locality=(1.0 0.0001)
 
-	      done
+for i in {0..3}
+	  do 
+		  var=$((2**(i+1))) 
+		  for t in ${locality[@]} 
+		  	do
+		  sed "s/UGRAD_SPATIAL_LOCALITY/${var}/" input_template > input_sub 
+		  sed "s/UGRAD_TEMPORAL_LOCALITY/${t}/" input_sub > input
+		  gcc gen.pub.c -lm -w > /dev/null
+		  ./a.out
+		  gcc Apex.c -lm -w > /dev/null
+		  sudo perf stat -B -d -d -d ./a.out > /dev/null
+	  done
+done
