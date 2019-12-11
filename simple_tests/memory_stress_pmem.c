85264cc4a5afb87b14e5d47150689411bce6e3f3
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-11 12:51:57 -0800

    more pmem tweak

--- simple_tests/memory_stress_pmem.c
+++ simple_tests/memory_stress_pmem.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-//This program allocates some number of gigabytes of longs and then randomly write and reads from them with zero locality
+//This program ns some number of gigabytes of longs and then randomly write and reads from them with zero locality
 //Build to test if huge pages is installed correct. Would see new zero dTLB misses if 1GB pages are working.
 //
 //Use: compile and pass number of GB wanted, if no arguments are passed this defaults to 4GB can add 2nd parameter "quiet" to prevent printing
@@ -18,33 +18,35 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 int main(int argc, char **argv) {
-    size_t allocate = 1024 * 1024 * 1024;
+    size_t n = 4UL * 1024 * 1024 * 1024;
     if (argc == 3) {
         int input = atoi(argv[1]);
-        allocate = (size_t) input * 1024 * 1024 * 1024;
+        n = (size_t) input * 1024 * 1024 * 1024;
     }
-    printf("attempting to allocate %ld bytes\n...  then will randomly write for ~5 seconds\n\n", allocate);
+    printf("attempting to n %ld bytes\n...  then will randomly write for ~5 seconds\n\n", n);
     int fd = open("/dev/dax0.0", O_RDWR);
     if (fd == -1) {
         perror("failed to open devdax\n");
     };
-    uint8_t *arr = mmap(NULL, allocate, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    uint8_t *arr = mmap(NULL, n, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
 //    uint_8t *arr = mmap(NULL, 19UL * 1024 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);//x140000000);
     if (arr == MAP_FAILED) {
         perror("mmap failed\n");
     }
-    for (int i = 0; i < MIN(allocate, 100 * 1024 * 1024); i++) {
+    for (int i = 0; i < MIN(n, 4UL * 1024 * 1024 * 1024); i++) {
         //print every now and then
         if (i % 100000 == 0 && argc < 3) {
-            printf("%d", arr[rand() % allocate]);
+            printf("%d", arr[rand() % n]);
         }
         //read forced with write
-        if (arr[rand() % allocate] == rand() % 255) {
-            arr[i] = rand() % 255;
+        size_t rand_num = ((size_t) rand() << 16) | (size_t) rand();
+        if (arr[rand_num % n] % 10 < 5) { //50% chance of write
+            arr[i] = rand_num;
         }
         //write
-        arr[rand() % allocate] = i;
+        arr[rand() % n] = i;
     }
-    munmap(arr, 19UL * 1024 * 1024 * 1024);
+    munmap(arr, n);
+//    munmap(arr, 19UL * 1024 * 1024 * 1024);
 }
