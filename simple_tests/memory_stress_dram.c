b55487ac07729b83901e8fe0fb9bc6406a692249
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-11 12:45:13 -0800

    update simpletest

--- simple_tests/memory_stress_dram.c
+++ simple_tests/memory_stress_dram.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 //This program allocates some number of gigabytes of longs and then randomly write and reads from them with zero locality
 //Build to test if huge pages is installed correct. Would see new zero dTLB misses if 1GB pages are working.
 //
@@ -12,35 +11,35 @@
 //  ./a.out 50 quite
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-int main(int argc, char **argv)
-{
-	int def = 4;
-	long allocate = 1024;	
-	long *arr;
-	int input;
-	if (argc < 2){		
-	 	allocate = ((long) def) * (1024*1024*1024) / sizeof (long); 
-	}else if (input = atoi(argv[1])){
-		allocate = ((long) input) * (1024*1024*1024) / sizeof (long); 
-	}
-	printf("attempting to allocate %ld bytes\n...  then will randomly write for ~5 seconds\n\n", allocate * sizeof(long));
-	if(arr = calloc(allocate, sizeof(long))){
-		for(int i = 0; i <MIN(allocate, 100*1000*1000) ; i++){
-		        //print every now and then
-			if(i % 10000 == 0 && argc < 3){
-			       printf( "%ld", arr[rand() % (allocate -1)]);
-			}
-			//read forced with write 
-			if(arr[rand() % (allocate - 1)] == rand() % 10){
-				arr[i] = rand() % 10;		
-			}
-			//write
-			arr[rand() % (allocate -1)] = i; 
-		}
-		free(arr);
-	}else{
-		printf("malloc failed");
-	}
-	
-	return 0;
+int main(int argc, char **argv) {
+    int def = 4;
+    long allocate = 1024;
+    long *arr;
+    int input;
+    if (argc < 2) {
+        allocate = ((long) def) * (1024 * 1024 * 1024) / sizeof(long);
+    } else if (input = atoi(argv[1])) {
+        allocate = ((long) input) * (1024 * 1024 * 1024) / sizeof(long);
+    }
+    printf("attempting to allocate %ld bytes\n...  then will randomly write for ~5 seconds\n\n",
+           allocate * sizeof(long));
+    if (arr = calloc(allocate, sizeof(long))) {
+        for (int i = 0; i < MIN(allocate, 100 * 1000 * 1000); i++) {
+            //print every now and then
+            if (i % 10000 == 0 && argc < 3) {
+                printf("%ld", arr[rand() % (allocate - 1)]);
+            }
+            //read forced with write
+            if (arr[rand() % (allocate - 1)] == rand() % 10) {
+                arr[i] = rand() % 10;
+            }
+            //write
+            arr[rand() % (allocate - 1)] = i;
+        }
+        free(arr);
+    } else {
+        printf("malloc failed");
+    }
+
+    return 0;
 }
--- simple_tests/memory_stress_pmem.c
+++ simple_tests/memory_stress_pmem.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,42 +10,41 @@
 //This program allocates some number of gigabytes of longs and then randomly write and reads from them with zero locality
 //Build to test if huge pages is installed correct. Would see new zero dTLB misses if 1GB pages are working.
 //
-//Use: compile and pass number of GB wanted, if no arguments are passed this defaults to 4GB can add 2nd paramater "quite" to prevent printing
+//Use: compile and pass number of GB wanted, if no arguments are passed this defaults to 4GB can add 2nd parameter "quiet" to prevent printing
 //
 //  ./a.out 
 //  ./a.out 25
-//  ./a.out 50 quite
+//  ./a.out 50 quiet
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
-int main(int argc, char **argv)
-{
-	int def = 4;
-	long allocate = 1024;	
-	int input;
-	if (argc < 2){		
-	 	allocate = ((long) def) * (1024*1024*1024) / sizeof (long); 
-	}else if (input = atoi(argv[1])){
-		allocate = ((long) input) * (1024*1024*1024) / sizeof (long); 
-	}
-	printf("attempting to allocate %ld bytes\n...  then will randomly write for ~5 seconds\n\n", allocate * sizeof(long));
-	int fd = open("/dev/dax0.0", O_RDWR);
-	if(fd == -1){printf("failed to open devdax\n"); exit(1);};
-	//long *arr = mmap(NULL, allocate, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	long *arr = mmap(NULL, 19327352832, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);//x140000000);
-	if(arr == MAP_FAILED){
-		printf("mmap failed\n");
-		exit(1);
-	}
-	for(int i = 0; i <MIN(allocate, 100*1000*1000) ; i++){
-	        //print every now and then
-		if(i % 10000 == 0 && argc < 3){
-		       printf( "%ld", arr[rand() % (allocate -1)]);
-		}
-		//read forced with write 
-		if(arr[rand() % (allocate - 1)] == rand() % 10){
-			arr[i] = rand() % 10;		
-		}
-		//write
-		arr[rand() % (allocate -1)] = i; 
-	}
-	free(arr);
+
+int main(int argc, char **argv) {
+    size_t allocate = 1024 * 1024 * 1024;
+    if (argc == 3) {
+        int input = atoi(argv[1]);
+        allocate = (size_t) input * 1024 * 1024 * 1024;
+    }
+    printf("attempting to allocate %ld bytes\n...  then will randomly write for ~5 seconds\n\n", allocate);
+    int fd = open("/dev/dax0.0", O_RDWR);
+    if (fd == -1) {
+        perror("failed to open devdax\n");
+    };
+    uint8_t *arr = mmap(NULL, allocate, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+//    uint_8t *arr = mmap(NULL, 19UL * 1024 * 1024 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);//x140000000);
+    if (arr == MAP_FAILED) {
+        perror("mmap failed\n");
+    }
+    for (int i = 0; i < MIN(allocate, 100 * 1024 * 1024); i++) {
+        //print every now and then
+        if (i % 100000 == 0 && argc < 3) {
+            printf("%d", arr[rand() % allocate]);
+        }
+        //read forced with write
+        if (arr[rand() % allocate] == rand() % 255) {
+            arr[i] = rand() % 255;
+        }
+        //write
+        arr[rand() % allocate] = i;
+    }
+    munmap(arr, 19UL * 1024 * 1024 * 1024);
 }
