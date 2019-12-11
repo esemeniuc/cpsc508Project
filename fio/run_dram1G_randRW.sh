1a76b60df4b418d37783baaa302716870def9315
cs508 <cs508@hennessy.cs.ubc.ca>
2019-12-10 17:01:50 -0800

    update fio scripts

--- fio/run_dram1G_randRW.sh
+++ fio/run_dram1G_randRW.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo mkdir -p /mnt/ramdisk
 sudo mount -t tmpfs -o size=20G tmpfs /mnt/ramdisk
-fio dram_randrw1G_ramdisk.fio
+sudo perf stat -B -d -d -d -e dtlb_load_misses.miss_causes_a_walk fio dram_randrw1G_ramdisk.fio
 sudo umount /mnt/ramdisk
--- fio/run_dram1G_seq_read.sh
+++ fio/run_dram1G_seq_read.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo mkdir -p /mnt/ramdisk
 sudo mount -t tmpfs -o size=20G tmpfs /mnt/ramdisk
-fio dram_seqr1G.fio
+sudo perf stat -B -d -d -d -e dtlb_load_misses.miss_causes_a_walk fio dram_seqr1G.fio
 sudo umount /mnt/ramdisk
