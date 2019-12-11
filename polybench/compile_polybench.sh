c3ae3265bf9d44d383fbd04d3a0e45d0cfeef153
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-11 15:00:10 -0800

    clean polybench

---
+++ polybench/compile_polybench.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+POLYBENCH_TESTS_DIR=("linear-algebra/kernels/2mm"
+    "linear-algebra/kernels/3mm"
+    "linear-algebra/kernels/atax"
+    "linear-algebra/solvers/cholesky"
+		"linear-algebra/solvers/durbin"
+    "linear-algebra/solvers/lu"
+		"linear-algebra/solvers/trisolv"
+		"linear-algebra/solvers/gramschmidt")
+
+POLYBENCH_TESTS_SOURCE=("linear-algebra/kernels/2mm/2mm.c"
+		"linear-algebra/kernels/3mm/3mm.c"
+		"linear-algebra/kernels/atax/atax.c"
+		"linear-algebra/solvers/cholesky/cholesky.c"
+    "linear-algebra/solvers/durbin/durbin.c"
+    "linear-algebra/solvers/lu/lu.c"
+    "linear-algebra/solvers/trisolv/trisolv.c"
+    "linear-algebra/solvers/gramschmidt/gramschmidt.c")
+
+TEST_NAMES=("2mm_time" 
+    "3mm_time"
+    "atax_time"
+    "cholesky_time"
+    "durbin_time"
+    "lu_time"
+    "trisolv_time"
+    "gramschmidt_time")
+
+SRC_DIR=polybench-c-4.2.1-beta
+mkdir -p bin
+mkdir -p result_logs
+mkdir -p perf_results
+
+for ((i = 0; i < ${#POLYBENCH_TESTS_DIR[@]}; ++i)); do
+  gcc -O3 -I ${SRC_DIR}/utilities \
+          -I ${SRC_DIR}/${POLYBENCH_TESTS_DIR[i]} \
+          ${SRC_DIR}/utilities/polybench.c \
+          ${SRC_DIR}/${POLYBENCH_TESTS_SOURCE[i]} \
+          -DPOLYBENCH_TIME -o bin/${TEST_NAMES[i]} -lm
+done
---
+++ polybench/polybench_dram.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TEST_NAMES=("2mm_time"
+    "3mm_time"
+    "atax_time"
+    "cholesky_time"
+    "durbin_time"
+    "lu_time"
+    "trisolv_time"
+    "gramschmidt_time")
+
+for ((i = 0; i < ${#TEST_NAMES[@]}; ++i))
+do
+  perf stat -B -d -d -d -e dtlb_load_misses.miss_causes_a_walk ../wrapper-devdax.sh /dev/dax0.0 $((28*1024*1024*1024)) bin/${TEST_NAMES[i]} result_logs/${TEST_NAMES[i]}.log > perf_results/${TEST_NAMES[i]}_perf.log
+done
