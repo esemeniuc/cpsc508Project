6ae467ea207dadd747e25512a4b38a14447b308a
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-11 15:11:10 -0800

    undo wrapper change

--- wrapper-comerge.sh
+++ wrapper-comerge.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
+#$3 is the executable to run
+#$4 is the output logging
 
 if [ "$#" -ne 4 ]; then
     echo "Illegal number of parameters"
 fi
-
-touch $3
-touch $4
 
 echo VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=libvmmalloc.so.1 $3 >> $4
 
