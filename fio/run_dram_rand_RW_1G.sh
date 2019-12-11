25120a6a429a79481347973eaf1070a9db4e57fa
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-11 10:07:00 -0800

    fix seq read and mmap

--- fio/run_dram_rand_RW_1G.sh
+++ fio/run_dram_rand_RW_1G.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-sudo mkdir -p /mnt/ramdisk
-sudo mount -t tmpfs -o size=20G tmpfs /mnt/ramdisk
 perf stat -B -d -d -d -e dtlb_load_misses.miss_causes_a_walk fio dram_rand_RW_1G.fio
-sudo umount /mnt/ramdisk
