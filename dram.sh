f19351bbfd583d94dd66ec48aa46dea879c5823f
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 12:56:19 -0800

    more scripty

--- dram.sh
+++ dram.sh
@@ -1,60 +1,24 @@
 #!/bin/bash
 set -x
-TLB_SIZES=(
-"1G"
-"2M"
-"4K"
-)
 
-#20 gig pool sizes
-HUGEADM_ALLOC_CMDS=(
-"sudo hugeadm --pool-pages-min 1G:20"
-"sudo hugeadm --pool-pages-min 2M:10240"
-"" #none for 4K
-)
+#apex
+cd apex-map/apex-map || exit
+./apex_dram.sh
+cd ..
 
-HUGECTL_CMDS=(
-"hugectl --heap=1073741824"
-"hugectl --heap=2097152"
-"" #no hugectl intercept needed for 4K
-)
+#fio
+cd fio || exit #script expects to be in directory
+./dram.sh
+cd ..
 
-for ((i = 0; i < ${#TLB_SIZES[@]}; ++i)); do
-    export HUGECTL_CMD=${HUGECTL_CMDS[i]}
-    export TLB_SIZE=${TLB_SIZES[i]}
+#polybench
+cd polybench || exit
+./compile_polybench.sh
+./polybench_dram.sh
+rm -rf bin
+cd ..
 
-    #reset pools at each iteration
-    sudo hugeadm --pool-pages-max 2M:0
-    sudo hugeadm --pool-pages-max 1G:0
-    if [ -n "${HUGEADM_ALLOC_CMDS[i]}" ]; then #don't run empty command
-        bash -c "${HUGEADM_ALLOC_CMDS[i]}"
-    fi
-
-    #apex
-    cd apex-map/apex-map || exit
-    ./apex_dram.sh
-    cd ..
-
-    #fio
-    cd fio || exit #script expects to be in directory
-    ./dram.sh
-    cd ..
-
-    #polybench
-    cd polybench || exit
-    ./compile_polybench.sh
-    ./polybench_dram.sh
-    rm -rf bin
-    cd ..
-
-    #simple_tests
-    cd simple_tests || exit
-    ./simple_dram.sh
-    cd ..
-
-done
-
-#sudo ndctl create-namespace -e namespace0.0 -m devdax -a 4K -f
-
-#sudo kexec -l /boot/vmlinuz-5.3.12-300.local.fc31.x86_64 --initrd=/boot/initramfs-5.3.12-300.local.fc31.x86_64.img --append='BOOT_IMAGE=(hd0,gpt2)/vmlinuz-5.3.12-300.local.fc31.x86_64 root=UUID=24769c34-6aa9-4fe5-8be1-5e6fb7a289e2 ro rhgb quiet memmap=32G!4G'
-#sudo systemctl kexec
+#simple_tests
+cd simple_tests || exit
+./simple_dram.sh
+cd ..
---
+++ full_run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#20 gig pool sizes
+
+SCRIPT=`realpath $0`
+#SCRIPTPATH=`dirname $SCRIPT`
+
+if [ ! -f /root/state0 ]; then
+    echo "RUNNING SETUP"
+    #./setup.sh
+    cat /proc/cmdline > backup_cmdline
+    echo "[Unit]
+Description=Example systemd service.
+
+[Service]
+Type=simple
+ExecStart=/bin/bash ${SCRIPT}
+
+[Install]
+WantedBy=multi-user.target"
+
+    sudo touch /root/state0
+fi
+
+#4K
+if [ ! -f /root/dram4K ]; then
+    export HUGECTL_CMD=""
+    export TLB_SIZE="4K"
+    ./dram.sh
+    sudo touch /root/dram4K
+    kexec -e "$(cat backup_cmdline) default_hugepagesz=2M"
+    sudo systemctl kexec
+fi
+
+#2M
+if [ ! -f /root/dram2M ]; then
+    export HUGECTL_CMD="hugectl --heap=2097152"
+    export TLB_SIZE="2M"
+    sudo hugeadm --pool-pages-min 2M:10240
+    ./dram.sh
+    sudo touch /root/dram2M
+    sudo hugeadm --pool-pages-max 2M:0
+    kexec -e "$(cat backup_cmdline) default_hugepagesz=1G"
+    sudo systemctl kexec
+fi
+
+#1G
+if [ ! -f /root/dram1G ]; then
+    export HUGECTL_CMD="hugectl --heap=1073741824"
+    export TLB_SIZE="1G"
+    sudo hugeadm --pool-pages-min 1G:20
+    ./dram.sh
+    sudo touch /root/dram1G
+    sudo hugeadm --pool-pages-max 1G:0
+    kexec -e "$(cat backup_cmdline) mmemmap=32G!4G"
+    sudo systemctl kexec
+fi
+
+#sudo ndctl create-namespace -e namespace0.0 -m devdax -a 4K -f
+
+#sudo kexec -l /boot/vmlinuz-5.3.12-300.local.fc31.x86_64 --initrd=/boot/initramfs-5.3.12-300.local.fc31.x86_64.img --append='BOOT_IMAGE=(hd0,gpt2)/vmlinuz-5.3.12-300.local.fc31.x86_64 root=UUID=24769c34-6aa9-4fe5-8be1-5e6fb7a289e2 ro rhgb quiet memmap=32G!4G'
+#sudo systemctl kexec
