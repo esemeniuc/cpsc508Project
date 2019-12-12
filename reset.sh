b21ea344b6df6d51e4c857293d85a25775913644
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 13:40:40 -0800

    update clean script

--- reset.sh
+++ reset.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+sudo hugeadm --pool-pages-max 2M:0
+sudo hugeadm --pool-pages-max 1G:0
+
 find . -type d -name perf_results -exec rm -rf {} \; #delete all folders named 'perf_results'
