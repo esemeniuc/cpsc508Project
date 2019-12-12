386ef7eb303cee330917fbfbdb4c143a6c6e54a8
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 15:02:34 -0800

    catch stderr

--- reset.sh
+++ reset.sh
@@ -4,6 +4,6 @@ sudo hugeadm --pool-pages-max 2M:0
 sudo hugeadm --pool-pages-max 1G:0
 sudo systemctl disable benchmark
 sudo rm /etc/systemd/system/benchmark.service
-sudo rm state_setup state_dram4K state_dram2M state_dram1G
+sudo rm state_setup state_dram4K state_dram2M state_dram1G output.txt
 
 find . -type d -name perf_results -exec rm -rf {} \; #delete all folders named 'perf_results'
