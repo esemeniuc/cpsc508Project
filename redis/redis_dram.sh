e184bb24e1a2d71ac85e95783788772ade0bae96
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 23:25:19 -0800

    more redis timeout

--- redis/redis_dram.sh
+++ redis/redis_dram.sh
@@ -3,7 +3,7 @@
 mkdir -p perf_results
 mkdir -p redis_results
 sudo perf stat -ddd -x '|' -e task-clock -e dtlb_load_misses.miss_causes_a_walk --append -o perf_results/dram_${TLB_SIZE}.log ${HUGECTL_CMD} redis-server &
-sleep 5 #let server boot up
+sleep 10 #let server boot up
 redis-benchmark -n 10000 -r 100 -t GET,SET -d 512000 --csv >> redis_results/dram_${TLB_SIZE}.log
 sudo killall redis-server
 rm -f dump.rdb
