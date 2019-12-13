6cef58c0357dea934e02a1864b16ddf4463ee5cf
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-13 06:11:48 -0800

    more clean

--- reset.sh
+++ reset.sh
@@ -2,6 +2,7 @@
 
 sudo hugeadm --pool-pages-max 2M:0
 sudo hugeadm --pool-pages-max 1G:0
+sudo systemctl stop benchmark
 sudo systemctl disable benchmark
 sudo rm /etc/systemd/system/benchmark.service
 sudo rm state_setup state_dram4K state_dram2M state_dram1G state_pmem output.txt
