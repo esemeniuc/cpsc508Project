cc10e379b47834fa95d3ae09764dec179b37ff3a
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-13 08:16:57 -0800

    hack for redis

--- redis/redis_pmem.sh
+++ redis/redis_pmem.sh
@@ -2,7 +2,7 @@
 
 mkdir -p perf_results
 mkdir -p redis_results
-(sudo perf stat -ddd -x '|' -e task-clock -e dtlb_load_misses.miss_causes_a_walk --append -o perf_results/pmem_${TLB_SIZE}.log ../wrapper-devdax.sh /dev/dax0.0 $((28*1024*1024*1024)) redis-server ../output.txt) &
+VMMALLOC_POOL_DIR=/dev/dax0.0 VMMALLOC_POOL_SIZE=30064771072 LD_PRELOAD=/usr/local/lib64/vmem_debug/libvmmalloc.so.1.0.0 redis-server &
 sleep 10 #let server boot up
 redis-benchmark -n 8000 -r 100 -t GET,SET -d 512000 --csv >> redis_results/pmem_${TLB_SIZE}.log
 sudo killall redis-server
