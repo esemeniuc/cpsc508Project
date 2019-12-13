748f6f635fb3c4479a6549a733867ea31b8fa4f1
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 22:36:01 -0800

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-sudo dnf install -y kexec-tools \
-python2 \ #for hugeadm
-perf \
-ndctl \
-redis \
-psmisc \ #for killall
-@development-tools
+sudo dnf install -y kexec-tools python2 perf ndctl redis psmisc @development-tools autoconf
 
 #backend for hugepages
 wget http://download-ib01.fedoraproject.org/pub/fedora/linux/releases/30/Everything/x86_64/os/Packages/l/libhugetlbfs-2.20-8.fc29.x86_64.rpm
@@ -16,7 +10,10 @@ sudo rpm -i libhugetlbfs-2.20-8.fc29.x86_64.rpm
 wget http://download-ib01.fedoraproject.org/pub/fedora/linux/releases/30/Everything/x86_64/os/Packages/l/libhugetlbfs-utils-2.20-8.fc29.x86_64.rpm
 sudo rpm -i libhugetlbfs-utils-2.20-8.fc29.x86_64.rpm
 
-cd vmem/src/ || exit
+#git clone https://github.com/esemeniuc/cpsc508Project.git
+#cd cpsc508Project
+
+cd vmem || exit
 make
 sudo make install
 
