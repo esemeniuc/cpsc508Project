5e43d6e2a2a279a1ab172bbd0fc019c7518c86aa
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-13 07:11:27 -0800

    add fio

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #python2 for hugeadm
-sudo dnf install -y kexec-tools python2 perf ndctl redis psmisc @development-tools autoconf ccache
+sudo dnf install -y kexec-tools python2 perf ndctl redis psmisc @development-tools autoconf ccache fio
 
 #backend for hugepages
 wget -nc http://download-ib01.fedoraproject.org/pub/fedora/linux/releases/30/Everything/x86_64/os/Packages/l/libhugetlbfs-2.20-8.fc29.x86_64.rpm
