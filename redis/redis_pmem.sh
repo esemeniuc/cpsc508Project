7eb1d29b6603b06a14dccfed1a4a419ba1b6321b
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 20:14:35 -0800

    add redis bench

---
+++ redis/redis_pmem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p perf_results
+mkdir -p redis_results
+sudo perf stat -ddd -x '|' -e task-clock -e dtlb_load_misses.miss_causes_a_walk --append -o perf_results/pmem_${TLB_SIZE}.log ${HUGECTL_CMD} redis-server &
+redis-benchmark -n 10000 -r 100 -t GET,SET -d 512000 --csv >> redis_results/pmem_${TLB_SIZE}.log
+sudo killall redis-server
