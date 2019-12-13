42fa4b528fd7a473893f25e2225c2ccb41f1bae6
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 21:18:41 -0800

    update setup

--- setup.sh
+++ setup.sh
@@ -16,10 +16,7 @@ sudo rpm -i libhugetlbfs-2.20-8.fc29.x86_64.rpm
 wget http://download-ib01.fedoraproject.org/pub/fedora/linux/releases/30/Everything/x86_64/os/Packages/l/libhugetlbfs-utils-2.20-8.fc29.x86_64.rpm
 sudo rpm -i libhugetlbfs-utils-2.20-8.fc29.x86_64.rpm
 
-#To use hugeadm, run to check that you have 2MB and 1GB pages supported:
-#hugeadm --pool-list 
-#and listed as zero zero
-#increase the min 2mb pages to 32GB of pages : sudo hugeadm --pool-pages-min 2MB:16384
-#Make a new directory for your page size: sudo mkdir -p /mnt/hugetlbfs-2MB
-#Then, mount: sudo mount -t hugetlbfs none -o pagesize=2MB
+cd vmem/src/ || exit
+make
+sudo make install
 
--- wrapper-devdax.sh
+++ wrapper-devdax.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #$3 is the executable to run
 #$4 is the output logging
+set -x
 
 if [ "$#" -ne 4 ]; then
     echo "Illegal number of parameters"
 fi
 
-echo VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=/home/cs508/cpsc508Project/vmem/src/debug/libvmmalloc.so $3 >> $4
-
-VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=/home/cs508/cpsc508Project/vmem/src/debug/libvmmalloc.so $3 >> $4
+VMMALLOC_POOL_DIR=$1 VMMALLOC_POOL_SIZE=$2 LD_PRELOAD=/usr/lib64/libvmmalloc.so.1 $3 >> $4
