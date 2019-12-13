d8aede5b78c8bf033f06dbf0e489e768977e3f8f
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 23:18:23 -0800

    more reset

--- reset.sh
+++ reset.sh
@@ -5,6 +5,7 @@ sudo hugeadm --pool-pages-max 1G:0
 sudo systemctl disable benchmark
 sudo rm /etc/systemd/system/benchmark.service
 sudo rm state_setup state_dram4K state_dram2M state_dram1G state_pmem output.txt
+rm -f apex-map/Apex.c apex-map/input
 
 find . -type d -name perf_results -exec rm -rf {} \; #delete all folders named 'perf_results'
 find . -type d -name fio_results -exec rm -rf {} \; #delete all folders named 'fio_results'
