8ad2415453a4ffece7a0755de89b8fe5126a25fc
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-12-12 19:00:17 -0800

    add looping

--- full_run.sh
+++ full_run.sh
@@ -3,6 +3,7 @@
 set -x
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
+LOOP_COUNT=1
 
 cd ${SCRIPTPATH} || exit #script expects to be in git root
 
@@ -23,8 +24,39 @@ WantedBy=multi-user.target
 EOF
 
     touch state_setup
-    sudo systemctl enable --now benchmark.service
+    sudo systemctl enable benchmark.service
+    sudo kexec -l /boot/vmlinuz-$(uname -r) --initrd /boot/initramfs-$(uname -r).img --append "$(cat backup_cmdline) memmap=32G!4G"
+    sudo systemctl kexec
     exit 0 #use to stop race until reboot kicks in
+fi
+
+#pmem
+if [ ! -f state_pmem ]; then
+    export HUGECTL_CMD=""
+    echo "RUNNING 4K pmem"
+    export TLB_SIZE="4K"
+    sudo ndctl create-namespace -e namespace0.0 -m devdax -a 4K -f
+    for _ in $(seq 1 $LOOP_COUNT); do
+        ./pmem.sh
+    done
+
+    echo "RUNNING 2M pmem"
+    export TLB_SIZE="2M"
+    sudo ndctl create-namespace -e namespace0.0 -m devdax -a 2M -f
+    for _ in $(seq 1 $LOOP_COUNT); do
+        ./pmem.sh
+    done
+
+    echo "RUNNING 1G pmem"
+    export TLB_SIZE="1G"
+    sudo ndctl create-namespace -e namespace0.0 -m devdax -a 1G -f
+    for _ in $(seq 1 $LOOP_COUNT); do
+        ./pmem.sh
+    done
+
+    touch state_pmem
+    sudo shutdown -r now
+    exit 0
 fi
 
 #4K dram
@@ -32,7 +64,10 @@ if [ ! -f state_dram4K ]; then
     echo "RUNNING 4K dram"
     export HUGECTL_CMD=""
     export TLB_SIZE="4K"
-    ./dram.sh
+    for _ in $(seq 1 $LOOP_COUNT); do
+        ./dram.sh
+    done
+
     touch state_dram4K
     sudo kexec -l /boot/vmlinuz-$(uname -r) --initrd /boot/initramfs-$(uname -r).img --append "$(cat backup_cmdline) default_hugepagesz=2M"
     sudo systemctl kexec
@@ -46,7 +81,9 @@ if [ ! -f state_dram2M ]; then
     export TLB_SIZE="2M"
     sudo hugeadm --pool-pages-min 2M:512
     sudo hugeadm --pool-pages-max 2M:20480 #allocate 40GB
-    ./dram.sh
+    for _ in $(seq 1 $LOOP_COUNT); do
+        ./dram.sh
+    done
     touch state_dram2M
     sudo hugeadm --pool-pages-max 2M:0
     sudo kexec -l /boot/vmlinuz-$(uname -r) --initrd /boot/initramfs-$(uname -r).img --append "$(cat backup_cmdline) default_hugepagesz=1G"
@@ -60,30 +97,12 @@ if [ ! -f state_dram1G ]; then
     export HUGECTL_CMD="hugectl --heap"
     export TLB_SIZE="1G"
     sudo hugeadm --pool-pages-min 1G:25 #stop crashes for OOM
-    ./dram.sh
+    for _ in $(seq 1 $LOOP_COUNT); do
+        ./dram.sh
+    done
     touch state_dram1G
     sudo hugeadm --pool-pages-max 1G:0
-    sudo kexec -l /boot/vmlinuz-$(uname -r) --initrd /boot/initramfs-$(uname -r).img --append "$(cat backup_cmdline) memmap=32G!4G"
-    sudo systemctl kexec
     exit 0
 fi
 
-#pmem
-if [ ! -f state_pmem ]; then
-    export HUGECTL_CMD=""
-    echo "RUNNING 4K pmem"
-    export TLB_SIZE="4K"
-    sudo ndctl create-namespace -e namespace0.0 -m devdax -a 4K -f
-    ./pmem.sh
 
-    echo "RUNNING 2M pmem"
-    export TLB_SIZE="2M"
-    sudo ndctl create-namespace -e namespace0.0 -m devdax -a 2M -f
-    ./pmem.sh
-
-    echo "RUNNING 1G pmem"
-    export TLB_SIZE="1G"
-    sudo ndctl create-namespace -e namespace0.0 -m devdax -a 1G -f
-    ./pmem.sh
-    touch state_pmem
-fi
