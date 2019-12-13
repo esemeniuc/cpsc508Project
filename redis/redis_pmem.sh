027439caa021f21105ab5b0819a16fff8af32df3
cs508 <cs508@hennessy.cs.ubc.ca>
2019-12-12 21:03:34 -0800

    fix redis pmem

--- redis/redis_pmem.sh
+++ redis/redis_pmem.sh
@@ -2,7 +2,7 @@
 
 mkdir -p perf_results
 mkdir -p redis_results
-sudo perf stat -ddd -x '|' -e task-clock -e dtlb_load_misses.miss_causes_a_walk --append -o perf_results/pmem_${TLB_SIZE}.log ../wrapper-devdax.sh /dev/dax0.0 $((28*1024*1024*1024)) redis-server ../output.txt
+(sudo perf stat -ddd -x '|' -e task-clock -e dtlb_load_misses.miss_causes_a_walk --append -o perf_results/pmem_${TLB_SIZE}.log ../wrapper-devdax.sh /dev/dax0.0 $((28*1024*1024*1024)) redis-server ../output.txt) &
 sleep 5 #let server boot up
 redis-benchmark -n 10000 -r 100 -t GET,SET -d 512000 --csv >> redis_results/pmem_${TLB_SIZE}.log
 sudo killall redis-server
